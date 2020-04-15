6b805817a985a0944c58fbe19dc0a4b4620866e7
jhony Guaman <jhonyguaman49@gmail.com>
2020-04-14 22:40:40 -0500

    fix issue 7

--- push.js
+++ push.js
@@ -7,8 +7,6 @@ let vapidKeys = {
  
 push.setVapidDetails('mailto:perdoalex0121@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey)
 
-let subs = {
-
-};
+let subs = {"endpoint":"https://fcm.googleapis.com/fcm/send/cb-Z070_9gc:APA91bFZbc-Jg2RcxoH7C3pdSbVNRfjLCVyNBRr2BmezQA7GbSMp0St8CsiqqrGJYdL1EXH1_ktI85SkiOcBGone6Vi51406HnS3SQIljSinIOw6AtlIBCqmRphW2nxvK9zOZLIOf8LC","expirationTime":null,"keys":{"p256dh":"BHEUCisGf514LYHhUIKGWGBfy_A1XCM5PyO0gwyd0rGSQBuoaExwZAwUWu7hiSP4DJbJXTaM6YYY9V687L1l5tw","auth":"68p1AR3zAiEe0ERJFg1Hqw"}};
 push.sendNotification(subs, 'Mensaje de prueba')
 //console.log(vapidKeys)
--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('push', () => {
-	self.registration.sendNotification('Cursos nuevos', {})
+self.addEventListener('push', e => {
+	var options = {
+		body: 'Esta notificacion fue generada desde push',
+		icon: 'images/example.png',
+		vibrate: [100,50,100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: '2'
+		},
+		actions: [
+			{action: 'explore', title: 'Explora este nuevo Mundo', icon:'images/checkmark.png'},
+			{action: 'close': title: 'Cerrar', icon: 'images/xmark.png'}
+		]
+	};
+	e.waitUntil(
+		self.registration.sendNotification('Cursos nuevos', options)
+		);
+	
 })
